f5f62217e8e0b4fd651ffaea12e69fbb717fc8da
Adevalter <adevalter@hotmail.com>
2024-09-21 19:30:13 -0300

    implementado crud empresa

--- src/main/java/br/com/adeweb/repasse/domain/services/EmpresaService.java
+++ src/main/java/br/com/adeweb/repasse/domain/services/EmpresaService.java
@@ -3,6 +3,8 @@ package br.com.adeweb.repasse.domain.services;
 import br.com.adeweb.repasse.data.models.EmpresaDTO;
 import br.com.adeweb.repasse.domain.entities.Empresa;
 import br.com.adeweb.repasse.domain.repositories.EmpresaRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
@@ -11,16 +13,42 @@ import org.springframework.stereotype.Service;
 public class EmpresaService {
     @Autowired
     private  EmpresaRepository empresaRepository;
-
+    @Autowired
+    private ModelMapper modelMapper;
 
     public Page<EmpresaDTO> findAll(Pageable pageable) {
         Page<Empresa> empresas = empresaRepository.findAll(pageable);
-        return empresas.map(empresa -> new EmpresaDTO(empresa.getId(),
-                empresa.getRazao(), empresa.getFantasia(),
-                empresa.getEmail(),empresa.getCelular(),empresa.getStatus()));
+        return empresas.map(empresa -> convertToDto(empresa));
     }
-    public Empresa salvar(Empresa empresa){
 
-        return empresa;
+    public EmpresaDTO buscaPorId(Long id){
+        Empresa empresa = empresaRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        return convertToDto(empresa);
     }
+
+    public EmpresaDTO salvar(EmpresaDTO empresaDTO){
+        Empresa empresa = convertToEmpresa(empresaDTO);
+        empresa.setStatus(1);
+        empresaRepository.save(empresa);
+
+        return convertToDto(empresa);
+    }
+
+    public EmpresaDTO atualizaEmpresa(Long id, EmpresaDTO empresaDTO){
+        Empresa empresa = convertToEmpresa(empresaDTO);
+        empresa.setId(id);
+        empresa = empresaRepository.save(empresa);
+        return convertToDto(empresa);
+    }
+
+    private EmpresaDTO convertToDto(Empresa empresa){
+        return modelMapper.map(empresa, EmpresaDTO.class);
+    }
+
+    private Empresa convertToEmpresa(EmpresaDTO empresaDto){
+        return modelMapper.map(empresaDto, Empresa.class);
+    }
+//    private void excluirEmpresa(Long id){
+//        empresaRepository.deleteById(id);
+//    }
 }
